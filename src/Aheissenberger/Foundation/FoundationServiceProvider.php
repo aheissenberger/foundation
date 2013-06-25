46f91c8e419751319bc473b896a6539b8616970d
Andreas Heissenberger <andreas@heissenberger.at>
2013-06-25 11:05:08 +0200

    init

---
+++ src/Aheissenberger/Foundation/FoundationServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Aheissenberger\Foundation;
+
+use Illuminate\Support\ServiceProvider;
+
+class FoundationServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('aheissenberger/foundation');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
